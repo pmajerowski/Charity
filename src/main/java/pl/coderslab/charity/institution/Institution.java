1861853302a91acbe9715c81fabc595f3138dbff
pmajerowski <105345146+pmajerowski@users.noreply.github.com>
2023-03-06 17:51:36 +0100

    Institution entity created

---
+++ src/main/java/pl/coderslab/charity/institution/Institution.java
@@ -0,0 +1,38 @@
+package pl.coderslab.charity.institution;
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "institutions")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Institution {
+    @Id
+    @SequenceGenerator(
+            name = "institution_sequence",
+            sequenceName = "institution_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "institution_sequence"
+    )
+    private Long id;
+
+    @Size(max = 255)
+    private String name;
+
+    @Size(max = 1000)
+    private String description;
+
+    public Institution (String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+}
