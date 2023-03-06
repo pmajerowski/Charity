34fc70b838e4f9b3d427f2326ebc35fafc566bf6
pmajerowski <105345146+pmajerowski@users.noreply.github.com>
2023-03-06 18:58:29 +0100

    CategoryConfig updated - categories changed and translated to polish

--- src/main/java/pl/coderslab/charity/category/CategoryConfig.java
+++ src/main/java/pl/coderslab/charity/category/CategoryConfig.java
@@ -12,18 +12,24 @@ public class CategoryConfig {
     @Bean
     CommandLineRunner commandLineRunner(CategoryRepository categoryRepository) {
         return args -> {
-            Category clothesCategory = new Category("Clothes");
-            Category furnitureCategory = new Category("Furniture");
-            Category toysCategory = new Category("Toys");
-            Category kitchenCategory = new Category("Kitchen");
-            Category hygieneCategory = new Category("Hygiene");
+            Category clothesCategory = new Category("Ubrania");
+            Category furnitureCategory = new Category("Meble");
+            Category toysCategory = new Category("Zabawki");
+            Category booksCategory = new Category("Książki");
+            Category kitchenCategory = new Category("Artykuły kuchenne");
+            Category hygieneCategory = new Category("Kosmetyki");
+            Category foodCategory = new Category("Żywność");
+            Category otherCategory = new Category("Inne");
 
             categoryRepository.saveAll(
                     List.of(clothesCategory,
+                            foodCategory,
                             furnitureCategory,
                             toysCategory,
                             kitchenCategory,
-                            hygieneCategory)
+                            booksCategory,
+                            hygieneCategory,
+                            otherCategory)
             );
         };
     }
