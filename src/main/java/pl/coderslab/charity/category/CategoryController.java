80bcdfe63720cf5f4a5a7ecae6175270981599ed
pmajerowski <105345146+pmajerowski@users.noreply.github.com>
2023-03-06 16:34:01 +0100

    getCategories action added to CategoryController

--- src/main/java/pl/coderslab/charity/category/CategoryController.java
+++ src/main/java/pl/coderslab/charity/category/CategoryController.java
@@ -5,11 +5,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("api/v1/category")
 @AllArgsConstructor
 public class CategoryController {
 
-//    @GetMapping
+    private final CategoryService categoryService;
+
+    @GetMapping("/list")
+    public List<Category> getCategories() {
+        return categoryService.getAllCategories();
+    }
 
 }
