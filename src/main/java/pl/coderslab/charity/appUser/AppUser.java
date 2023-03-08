e931a797fceb6d48aa3da6f7dd45b995d69250d0
pmajerowski <105345146+pmajerowski@users.noreply.github.com>
2023-03-08 12:22:58 +0100

    AppUser entity created

---
+++ src/main/java/pl/coderslab/charity/appUser/AppUser.java
@@ -0,0 +1,30 @@
+package pl.coderslab.charity.appUser;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class AppUser {
+    @Id
+    @SequenceGenerator(
+            name = "user_sequence",
+            sequenceName = "user_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "user_sequence"
+    )
+    private Long id;
+
+    private String name;
+
+}
