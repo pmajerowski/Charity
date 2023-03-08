c46c74f728ec8b65f868e2c33c521a9a9e054d8d
pmajerowski <105345146+pmajerowski@users.noreply.github.com>
2023-03-08 14:13:45 +0100

    Date validation added in app.js

--- src/main/webapp/resources/js/app.js
+++ src/main/webapp/resources/js/app.js
@@ -183,6 +183,15 @@ document.addEventListener("DOMContentLoaded", function () {
             }
 
             // STEP 4
+            const currentDate = new Date();
+
+            const year = currentDate.getFullYear();
+            const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+            const day = String(currentDate.getDate()).padStart(2, '0');
+            const minDate = `${year}-${month}-${day}`;
+
+            document.getElementById('date').setAttribute('min', minDate);
+
             const streetInput = document.getElementById('street');
             const cityInput = document.getElementById('city');
             const zipCodeInput = document.getElementById('zipCode');
