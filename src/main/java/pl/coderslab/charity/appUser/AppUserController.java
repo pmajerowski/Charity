d3e3f6fb0f62e1291de898b5c581557cd5e11d39
pmajerowski <105345146+pmajerowski@users.noreply.github.com>
2023-03-08 13:40:14 +0100

    AppUserController modified - added postMapping for registration, returns User for now

--- src/main/java/pl/coderslab/charity/appUser/AppUserController.java
+++ src/main/java/pl/coderslab/charity/appUser/AppUserController.java
@@ -1,11 +1,18 @@
 package pl.coderslab.charity.appUser;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@AllArgsConstructor
 public class AppUserController {
+
+    private final AppUserService appUserService;
 
     @GetMapping("register")
     public String registerUser(Model model) {
@@ -13,5 +20,15 @@ public class AppUserController {
         model.addAttribute("user", user);
 
         return "registration";
+    }
+
+    @PostMapping("register")
+    @ResponseBody
+    public AppUser saveUser(AppUser user, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return null;
+        }
+        appUserService.saveUser(user);
+        return user;
     }
 }
--- src/main/webapp/WEB-INF/views/registration.jsp
+++ src/main/webapp/WEB-INF/views/registration.jsp
@@ -2,6 +2,7 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
 <%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 <!DOCTYPE html>
 <html lang="pl">
 
@@ -28,7 +29,7 @@
 
     <section class="login-page">
         <h2>Załóż konto</h2>
-        <form>
+        <form:form method="post" modelAttribute="user">
             <div class="form-group">
                 <input type="email" name="email" placeholder="Email" />
             </div>
@@ -43,7 +44,7 @@
                 <a href="login.html" class="btn btn--without-border">Zaloguj się</a>
                 <button class="btn" type="submit">Załóż konto</button>
             </div>
-        </form>
+        </form:form>
     </section>
 
     <%--FOOTER--%>
