941c2e1ae74025f5242aa030472f9568688fb8bf
pmajerowski <105345146+pmajerowski@users.noreply.github.com>
2023-03-08 12:33:33 +0100

    link to registration and controller action created

--- src/main/java/pl/coderslab/charity/HomeController.java
+++ src/main/java/pl/coderslab/charity/HomeController.java
@@ -3,7 +3,9 @@ package pl.coderslab.charity;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.appUser.AppUser;
 import pl.coderslab.charity.donation.DonationService;
 import pl.coderslab.charity.institution.Institution;
 import pl.coderslab.charity.institution.InstitutionService;
@@ -23,11 +25,17 @@ public class HomeController {
     public String homeAction(Model model){
         List<Institution> institutions = institutionService.getAllInstitutions();
         model.addAttribute("institutions", institutions);
-
         model.addAttribute("donationsNumber", donationService.numberOfDonations());
-
         model.addAttribute("bagsDonated", donationService.numberOfBagsDonated());
 
         return "index";
+    }
+
+    @GetMapping("register")
+    public String registerUser(Model model) {
+        AppUser user = new AppUser();
+        model.addAttribute("user", user);
+
+        return "registration";
     }
 }
