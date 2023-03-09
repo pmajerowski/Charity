70a5735459664092c7373966b6c603f649a54364
pmajerowski <105345146+pmajerowski@users.noreply.github.com>
2023-03-09 22:55:21 +0100

    css loads fine, registration works with pass hashing, login not working

--- src/main/java/pl/coderslab/charity/appuser/AppUserController.java
+++ src/main/java/pl/coderslab/charity/appuser/AppUserController.java
@@ -1,12 +1,19 @@
 package pl.coderslab.charity.appuser;
 
 import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -16,6 +23,10 @@ public class AppUserController {
     private final PasswordEncoder passwordEncoder;
 
     private final AppUserService appUserService;
+
+    private AuthenticationManager authenticationManager;
+
+    private BCryptPasswordEncoder encoder;
 
     private final UserDetailsService userDetailsService;
 
@@ -47,8 +58,20 @@ public class AppUserController {
     }
 
     @PostMapping("/login")
-    public String login(AppUser user, BindingResult bindingResult) {
+    public String login(@ModelAttribute("user") AppUser userCredentials, Model model) {
+        String email = userCredentials.getEmail();
+        String password = userCredentials.getPassword();
+        String encodedPassword = encoder.encode(password);
 
-        return "redirect:form";
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(email, encodedPassword);
+
+        try {
+            Authentication authentication = authenticationManager.authenticate(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            return "redirect:/";
+        } catch (AuthenticationException e) {
+            model.addAttribute("error", "Invalid email or password");
+            return "login";
+        }
     }
 }
