d29d18b65b35c391036ddafa86ae76d5826cbafb
pmajerowski <105345146+pmajerowski@users.noreply.github.com>
2023-03-11 12:37:39 +0100

    Edited register action to create appUser via userDTO with displaying errors in form

--- src/main/java/pl/coderslab/charity/appuser/AppUserController.java
+++ src/main/java/pl/coderslab/charity/appuser/AppUserController.java
@@ -8,8 +8,15 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import pl.coderslab.charity.dto.UserDTO;
+import pl.coderslab.charity.role.Role;
+import pl.coderslab.charity.role.RoleService;
+import pl.coderslab.charity.security.UserDetailsServiceImpl;
 
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
+import java.util.Objects;
+import java.util.Set;
 
 @Controller
 @AllArgsConstructor
@@ -19,34 +26,66 @@ public class AppUserController {
 
     private final AppUserService appUserService;
 
+    private final RoleService roleService;
+
     @GetMapping("register")
-    public String registerUser(Model model) {
-        AppUser user = new AppUser();
-        model.addAttribute("user", user);
+    public String registerUser(Model model, HttpSession session) {
+        if (session.getAttribute("userDTO") != null) {
+            return "registration";
+        }
+
+        UserDTO userDTO = new UserDTO();
+        model.addAttribute("userDTO", userDTO);
+        session.setAttribute("userDTO", userDTO);
 
         return "registration";
     }
 
     @PostMapping("register")
-    public String saveUser(@Valid AppUser user,
+    public String saveUser(@Valid UserDTO userDTO,
                            BindingResult bindingResult,
-                           @RequestParam(name = "password2") String password2) {
+                           Model model,
+                           HttpSession session) {
+
         if (bindingResult.hasErrors()) {
-            return null;
-        }
-        // TODO: Set<Roles>
-
-        // TODO: AppUserDTO jako model
-
-
-        if (user.getPassword() != password2) {
-
+            model.addAttribute(
+                    "err",
+                    "BINDING RESULT WRONG");
+            return "redirect:register";
         }
 
-        user.setPassword(
-                passwordEncoder.encode(user.getPassword())
+        if (appUserService.findByEmail(userDTO.getUsername()).isPresent()) {
+            userDTO.setError(
+                    "Istnieje już konto powiązane z tym adresem email"
+            );
+            session.setAttribute("userDTO", userDTO);
+            return "redirect:/register";
+        }
+
+        if (!Objects.equals(userDTO.getPassword(), userDTO.getPassword2())) {
+            userDTO.setError(
+                    "Podane hasła różnią się od siebie"
+            );
+            session.setAttribute("userDTO", userDTO);
+            return "redirect:/register";
+        }
+
+        session.removeAttribute("userDTO");
+
+        AppUser appUser = new AppUser();
+        appUser.setFirstName(userDTO.getFirstName());
+        appUser.setLastName(userDTO.getLastName());
+        appUser.setEmail(userDTO.getUsername());
+        appUser.setPassword(
+                passwordEncoder.encode(userDTO.getPassword())
         );
-        appUserService.saveUser(user);
+
+        Set<Role> roles = roleService.findRolesByName("ROLE_USER");
+        appUser.setRoles(roles);
+
+        appUser.setEnabled(true);
+
+        appUserService.saveUser(appUser);
         return "login";
     }
 
