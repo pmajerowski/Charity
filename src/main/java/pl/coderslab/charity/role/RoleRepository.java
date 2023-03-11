e511649bf9b457aa62cc5a6dbb2c1e7c158237e8
pmajerowski <105345146+pmajerowski@users.noreply.github.com>
2023-03-11 12:36:52 +0100

    created RoleRepository and RoleService

--- src/main/java/pl/coderslab/charity/role/RoleRepository.java
+++ src/main/java/pl/coderslab/charity/role/RoleRepository.java
@@ -1,8 +1,12 @@
 package pl.coderslab.charity.role;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Set;
+
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
-    // TODO: get name
+    Set<Role> findByName(String roleName);
 }
---
+++ src/main/java/pl/coderslab/charity/role/RoleService.java
@@ -0,0 +1,17 @@
+package pl.coderslab.charity.role;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+public class RoleService {
+
+    private final RoleRepository roleRepository;
+
+    public Set<Role> findRolesByName(String roleName) {
+        return roleRepository.findByName(roleName);
+    }
+}
