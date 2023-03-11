cd4939ade571b54899cff28ccf60ed0efd9e7b99
pmajerowski <105345146+pmajerowski@users.noreply.github.com>
2023-03-11 12:36:32 +0100

    created userDTO, added firstName, lastName and error

---
+++ src/main/java/pl/coderslab/charity/dto/UserDTO.java
@@ -0,0 +1,20 @@
+package pl.coderslab.charity.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class UserDTO {
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    private String password2;
+    private String error;
+
+}
