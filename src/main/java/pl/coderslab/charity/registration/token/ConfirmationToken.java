39d6ba850bbf9d71bd98ab40f848240a243b9873
pmajerowski <105345146+pmajerowski@users.noreply.github.com>
2023-03-12 13:44:21 +0100

    ConfirmationToken entity created

---
+++ src/main/java/pl/coderslab/charity/registration/token/ConfirmationToken.java
@@ -0,0 +1,59 @@
+package pl.coderslab.charity.registration.token;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import pl.coderslab.charity.appuser.AppUser;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+
+public class ConfirmationToken {
+    public ConfirmationToken(String token,
+                             LocalDateTime createdAt,
+                             LocalDateTime expiresAt,
+                             LocalDateTime confirmedAt,
+                             AppUser appUser) {
+        this.token = token;
+        this.createdAt = createdAt;
+        this.expiresAt = expiresAt;
+        this.confirmedAt = confirmedAt;
+        this.appUser = appUser;
+    }
+
+    @Id
+    @SequenceGenerator(
+            name = "confirm_token_sequence",
+            sequenceName = "confirm_token_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "confirm_token_sequence"
+    )
+    private Long id;
+
+    @Column(nullable = false)
+    private String token;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime expiresAt;
+
+    private LocalDateTime confirmedAt;
+
+    @ManyToOne
+    @JoinColumn(
+            nullable = false,
+            name = "app_user_id"
+    )
+    private AppUser appUser;
+
+}
