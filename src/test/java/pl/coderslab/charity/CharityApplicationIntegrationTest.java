f107a94fb27d81ed55992e5a65f079448c3eb953
pmajerowski <105345146+pmajerowski@users.noreply.github.com>
2023-03-19 15:49:05 +0100

    in progress

--- src/test/java/pl/coderslab/charity/CharityApplicationIntegrationTest.java
+++ src/test/java/pl/coderslab/charity/CharityApplicationIntegrationTest.java
@@ -2,26 +2,26 @@ package pl.coderslab.charity;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
-@SpringBootTest
-@AutoConfigureMockMvc
-public class CharityApplicationIntegrationTest {
+
+public class CharityApplicationIntegrationTest extends AbstractIntegrationTest {
+
+
 
     @Autowired
     private MockMvc mockMvc;
 
     @Test
     public void getNewView() throws Exception {
+        System.out.println(mySQLContainer.getJdbcUrl());
+        System.out.println(mySQLContainer.getUsername());
+        System.out.println(mySQLContainer.getDatabaseName());
+        System.out.println(mySQLContainer.getPassword());
 
         this.mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
