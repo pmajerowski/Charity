49113db3c3b969ed0c6e0349ddf92e9efb951335
pmajerowski <105345146+pmajerowski@users.noreply.github.com>
2023-03-19 18:00:18 +0100

    integration testing environment established

--- src/test/java/pl/coderslab/charity/CharityApplicationIntegrationTest.java
+++ src/test/java/pl/coderslab/charity/CharityApplicationIntegrationTest.java
@@ -7,30 +7,16 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-
-
-public class CharityApplicationIntegrationTest extends AbstractIntegrationTest {
-
-
+public class CharityApplicationIntegrationTest extends BaseIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @Test
     public void getNewView() throws Exception {
-        System.out.println(mySQLContainer.getJdbcUrl());
-        System.out.println(mySQLContainer.getUsername());
-        System.out.println(mySQLContainer.getDatabaseName());
-        System.out.println(mySQLContainer.getPassword());
-
         this.mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
                 .andExpect(model().attributeExists("institutions"))
                 .andExpect(view().name("index"));
-    }
-
-    @Test
-    public void shouldTest() {
-
     }
 }
