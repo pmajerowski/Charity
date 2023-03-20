38658ca5fc2389ae2e43d1d64869d152abc57a40
pmajerowski <105345146+pmajerowski@users.noreply.github.com>
2023-03-20 17:54:05 +0100

    DonationServiceIntegrationTest created, empty for now

---
+++ src/test/java/pl/coderslab/charity/integration/DonationServiceIntegrationTest.java
@@ -0,0 +1,20 @@
+package pl.coderslab.charity.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import pl.coderslab.charity.donation.DonationService;
+
+public class DonationServiceIntegrationTest extends BaseIntegrationTest {
+
+    @Autowired
+    private DonationService donationService;
+
+    @Test
+    public void should() {
+        // given
+
+        // when
+
+        // then
+    }
+}
